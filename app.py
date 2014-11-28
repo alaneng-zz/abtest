7f38ebf860c33ed805b8667e45555a4ebf25c72b
Alan Eng <aeng@stitchfix.com>
2014-11-28 15:26:52 -0800

    update app stuff

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for, request, make_response
+from flask import Flask, render_template, url_for, request, make_response, Response
 from scipy.stats import beta 
 import mpld3
 import matplotlib.pyplot as plt
 import numpy as np
+import io
+import base64
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
+app.config['DEBUG'] = True
 
 
 @app.route('/', methods=['GET'])
@@ -14,22 +16,24 @@ def landing():
     return render_template('index.html')
 
 @app.route('/stats', methods=['POST'])
-def create_stats():
+def plot_stats():
     data = request.form
     x = np.linspace(0.1,0.6,200)
-    y = beta.pdf(x, 1 + int(data['conversion_a']), 1 + int(data['allocation_a']) - int(data['conversion_a']))
+    y_a = beta.pdf(x, 1 + int(data['conversion_a']), 1 + int(data['allocation_a']) - int(data['conversion_a']))
+    y_b = beta.pdf(x, 1 + int(data['conversion_b']), 1 + int(data['allocation_b']) - int(data['conversion_b']))
 
-    plt.plot(x,y, label='a')
-    plt.savefig('mychart')
+    plt.plot(x, y_a, label='A')
+    plt.plot(x, y_b, label='B')
 
-    fig=Figure()
-    ax=fig.add_subplot(111)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
 
-    canvas=FigureCanvas(fig)
-    png_output = StringIO.StringIO()
-    canvas.print_png(png_output)
-    response=make_response(png_output.getvalue())
-    response.headers['Content-Type'] = 'image/png'
+    image_data = base64.b64encode(buf.read())
+
+    response = Response(response=image_data, status=200)
+    buf.close()
+    plt.close()
 
     return response
 
